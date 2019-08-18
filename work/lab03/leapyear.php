28564f7c923ca2ce77d82301fcace99f5f4df2b5
Daniel Coady <daniel.coady@outlook.com.au>
2019-08-18 15:43:04 +1000

    finished task 2 week 3

---
+++ work/lab03/leapyear.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="web programming :: leap year" />
+    <meta name="keywords" content="web, programming" />
+    <title>leap year</title>
+</head>
+<body>
+    <h1>web app dev - lab 3</h1>
+    <?php
+        if (isset($_GET["year"]))
+        {
+            $year = $_GET["year"];
+            if (is_numeric($year))
+            {
+                if ($year >= 0)
+                {
+                    if ($year == round($year)) // num is an int
+                    {
+                        $result = "standard";
+
+                        if ($year % 4 == 0)
+                            if ($year % 100 != 0)
+                                $result = "leap";
+                            elseif ($year % 400 == 0)
+                                $result = "leap";
+                        else
+                            $result = "standard";
+
+                        echo "<p>$year is a $result year</p>";
+                    }
+                    else // num is not an int
+                    {
+                        echo "<p>please enter an integer</p>";
+                    }
+                }
+                else
+                {
+                    echo "<p>please enter a positive number</p>";
+                }
+            }
+            else
+            {
+                echo "<p>please enter a number</p>";
+            }
+        }
+        else
+        {
+            echo "<p>please make an input</p>";
+        }
+    ?>
+    <p><a href="leapyearform.php">go back</a></p>
+</body>
+</html>
---
+++ work/lab03/leapyearform.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="web programming :: leap year" />
+    <meta name="keywords" content="web, programming" />
+    <title>leap year</title>
+</head>
+<body>
+    <h1>web app dev - lab 3</h1>
+    <form action="leapyear.php" method="GET">
+        <label for="year">year: </label>
+        <input type="text" name="year" />
+        <input type="submit" value="submit" />
+    </form>
+</body>
+</html>
