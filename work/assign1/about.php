9e1707ae27f0660c7a6437fdffbf1d81f139e760
Daniel Coady <daniel.coady@outlook.com.au>
2019-09-08 12:17:06 +1000

    added info to about page

--- work/assign1/about.php
+++ work/assign1/about.php
@@ -15,6 +15,21 @@
 
     <main class="container lead">
         <h1>about</h1>
+        <ul>
+            <li>currently, this server is running PHP version <?php echo phpversion(); ?></li>
+            <li>for this assignment, i have not completed/attempted the following tasks:</li>
+            <ul>
+                <li>uh oh</li>
+            </ul>
+            <li>for this site i have done the following extra things:</li>
+            <ul>
+                <li>used bootstrap</li>
+                <li>created custom styling with CSS</li>
+            </ul>
+        </ul>
+
+        <p>i have provided the following discussion on the discussion board:</p>
+        <img src="discussionboard.jpg" />
     </main>
 </body>
 </html>
