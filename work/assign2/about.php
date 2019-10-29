0ba6eb646b823b7b51cd36fc02116a951bf63434
Daniel Coady <daniel.coady@outlook.com.au>
2019-10-29 12:31:11 +1100

    mostly finished the about page

--- work/assign2/about.php
+++ work/assign2/about.php
@@ -15,6 +15,35 @@
 
     <main class="container lead">
     <h1>about</h1>
+    <ul>
+        <li>TODO: say what tasks i did not complete</li>
+        <li>for this assignment i have done the following extra tasks:
+        <ul>
+            <li>used bootstrap</li>
+            <li>created custom styling with CSS</li>
+            <li>created a user class and helper functions for easier user data management</li>
+        </ul></li>
+        <li>
+            the part that i had the most trouble with was getting the user class and it's
+            helper functions to a point where they were all working as intended. this
+            ended up being time well spent however since it was far easier to work with any
+            user data as a result.
+        </li>
+        <li>
+            currently the code base is a bit fragmented and out of order. there's certainly a
+            lot more repeated code than i would have liked, so more time spent earlier on
+            refactoring and sticking to cleaner code standards would be something very much
+            worth considering in the future.
+        </li>
+        <li>there are no additional features added to the site</li>
+        <li>links to some pages:
+        <ul>
+            <li><a href="friendslist.php">friends list</a> (note that if you are not logged in you will be sent to the login page)</li>
+            <li><a href="friendadd.php">add friends</a> (note that if you are not logged in you will be sent to the login page)</li>
+            <li><a href="index.php">home</a></li>
+        </ul></li>
+        <li>i did not participate on the discussion board for this assignment</li>
+    <ul>
     </main>
 </body>
 </html>
